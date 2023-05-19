b0f0f51564dcb1c9c6b35d8a8fb40c7d756c407e
bradfordwagner <wagner.bradford@gmail.com>
2023-05-18 23:05:37 -0400

    rename module

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
   features {}
 }
 
-module "hello_world" {
+module "acr" {
   source              = "git::https://github.com/bradfordwagner/tf-m-az-acr.git?ref=0.1.0"
   name                = var.name
   resource_group_name = var.resource_group_name
